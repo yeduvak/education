da73897acccbba5488255ee059f888db9a7b0a96
HNodir <81225920+HNodir@users.noreply.github.com>
2021-03-24 19:10:53 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+$(document).ready(function(){
+    
+$('.post-wrapper').slick({
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 5000,
+  nextArrow: $('.next'),
+  prevArrow: $('.prev'),
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }    
+  ]
+});
+    
+});
